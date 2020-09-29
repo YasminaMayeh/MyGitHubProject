24c3e5c4f642d1d22f97ef10ce8fe5bf69657392
yasminamayeh <Yasminmayeh@gmail.com>
2020-09-29 16:09:53 +0100

    added python lesson file

---
+++ lesson1.py
@@ -0,0 +1,58 @@
+# =============================================================================
+# Operators and operations
+# =============================================================================
+
+# +  add
+# -  subtract
+# /  divide
+# *  multiply
+
+# =  assignment operator
+
+# ** to the power of (exponent)
+
+# %  remainder (Modulus) 
+#      example: the number 1
+#                 in 11/5 = 2r1
+
+# // floor division
+#      example: the number 2
+#                 in 11/5 = 2r1
+
+# =============================================================================
+# VARIABLES
+# =============================================================================
+
+# PERSON: 150
+height = 150 # changed to 150
+weight = 80 
+
+# BMI = height/weight
+print(height/weight)
+
+# compare persons height
+# to tallest person in world
+#  tallest: 200cm
+# print(200-150)
+tallest_height = 201
+print(tallest_height - height)
+
+# print height
+print(height)
+
+# =============================================================================
+# Operators and variables
+# =============================================================================
+a = 3
+b = 5
+
+# 3^5 **
+print(3**5) 
+print(a**b)
+
+# remainder 
+print(a%b) # 3/5 = 0r3
+
+# floor division
+print(a//b) # 3/5 = 0r3
+
